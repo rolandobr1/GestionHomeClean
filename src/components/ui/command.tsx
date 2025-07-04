9e39c0463c4cdf2affa11c060b4b0a434799b9c5
Rolando Valdez Suero <valdezrolando93@gmail.com>
2025-07-04 14:51:32 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -113,16 +113,19 @@ CommandSeparator.displayName = CommandPrimitive.Separator.displayName
 const CommandItem = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof CommandPrimitive.Item>
->(({ className, ...props }, ref) => (
-  <CommandPrimitive.Item
-    ref={ref}
-    className={cn(
-      "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
-      className
-    )}
-    {...props}
-  />
-))
+>(({ className, ...props }, ref) => {
+  const { params, searchParams, ...rest } = props as any
+  return (
+    <CommandPrimitive.Item
+      ref={ref}
+      className={cn(
+        "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+        className
+      )}
+      {...rest}
+    />
+  )
+})
 
 CommandItem.displayName = CommandPrimitive.Item.displayName
 
