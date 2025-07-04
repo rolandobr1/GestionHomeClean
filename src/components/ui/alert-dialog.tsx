941893a80a5976272f449f7bbd418819f9af6c3f
Rolando Valdez Suero <valdezrolando93@gmail.com>
2025-07-04 13:04:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -101,29 +101,33 @@ AlertDialogDescription.displayName =
 const AlertDialogAction = React.forwardRef<
   React.ElementRef<typeof AlertDialogPrimitive.Action>,
   React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Action>
->(({ className, ...props }, ref) => (
-  <AlertDialogPrimitive.Action
+>(({ className, ...props }, ref) => {
+  const {params, searchParams, ...rest} = props as any;
+  return (<AlertDialogPrimitive.Action
     ref={ref}
     className={cn(buttonVariants(), className)}
-    {...props}
+    {...rest}
   />
-))
+  )
+})
 AlertDialogAction.displayName = AlertDialogPrimitive.Action.displayName
 
 const AlertDialogCancel = React.forwardRef<
   React.ElementRef<typeof AlertDialogPrimitive.Cancel>,
   React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Cancel>
->(({ className, ...props }, ref) => (
-  <AlertDialogPrimitive.Cancel
+>(({ className, ...props }, ref) => {
+  const {params, searchParams, ...rest} = props as any;
+  return (<AlertDialogPrimitive.Cancel
     ref={ref}
     className={cn(
       buttonVariants({ variant: "outline" }),
       "mt-2 sm:mt-0",
       className
     )}
-    {...props}
+    {...rest}
   />
-))
+  )
+})
 AlertDialogCancel.displayName = AlertDialogPrimitive.Cancel.displayName
 
 export {
